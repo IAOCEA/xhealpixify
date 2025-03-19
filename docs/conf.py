505edd2e18fbf1b53300d9defa1721de5dcbc2fe
quentin <quentinmaz@hotmail.fr>
2025-03-19 16:32:15 +0100

    improved documentation and its publication with readthedocs.

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,3 @@
-# -- Project information -----------------------------------------------------
 import datetime as dt
 
 project = "xarray-healpy"
@@ -7,50 +6,62 @@ initial_year = "2023"
 year = dt.datetime.now().year
 copyright = f"{initial_year}-{year}, {author}"
 
-# The root toctree document.
-root_doc = "index"
-
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
-    "sphinx.ext.extlinks",
+    "sphinx.ext.autosummary",
+    "sphinx.ext.autodoc",
+    "sphinx.ext.napoleon",
+    # "sphinx.ext.doctest",
+    # "autoapi.extension",
+    "sphinx_copybutton",
+    "myst_parser",
+    "nbsphinx",
     "sphinx.ext.intersphinx",
-    "IPython.sphinxext.ipython_directive",
-    "IPython.sphinxext.ipython_console_highlighting",
+    "nbsphinx_link",
+    "sphinx_remove_toctrees",
 ]
 
-extlinks = {
-    "issue": ("https://github.com/keewis/xarray-healpy/issues/%s", "GH%s"),
-    "pull": ("https://github.com/keewis/xarray-healpy/pull/%s", "PR%s"),
+napoleon_google_docstring = False
+napoleon_numpy_docstring = True
+napoleon_use_param = False
+napoleon_use_rtype = False
+napoleon_preprocess_types = True
+napoleon_type_aliases = {
+    # general terms
+    "sequence": ":term:`sequence`",
+    "iterable": ":term:`iterable`",
+    "callable": ":term:`callable`",
+    "dict_like": ":term:`dict-like <mapping>`",
+    "dict-like": ":term:`dict-like <mapping>`",
+    "path-like": ":term:`path-like <path-like object>`",
+    "mapping": ":term:`mapping`",
+    "file-like": ":term:`file-like <file-like object>`",
+    "any": ":py:class:`any <object>`",
+    # numpy terms
+    "array_like": ":term:`array_like`",
+    "array-like": ":term:`array-like <array_like>`",
+    "scalar": ":term:`scalar`",
+    "array": ":term:`array`",
+    "hashable": ":term:`hashable <name>`",
 }
 
-# Add any paths that contain templates here, relative to this directory.
+autosummary_generate = True
+autosummary_imported_members = False
+nbsphinx_execute = "never"
+
 templates_path = ["_templates"]
+exclude_patterns = ["_build", "Thumbs.db", ".DS_Store", ".ipynb_checkpoints"]
+remove_from_toctrees = ["generated/*"]
+suppress_warnings = ["config.cache"]
 
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = ["_build", "directory"]
-
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "sphinx_book_theme"
+pygments_style = "sphinx"
 
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["_static"]
-
-# -- Options for the intersphinx extension -----------------------------------
+html_static_path = []
 
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3/", None),
-    "sphinx": ("https://www.sphinx-doc.org/en/stable/", None),
+    "sphinx": ("https://www.sphinx-doc.org/en/master/", None),
+    "numpy": ("https://numpy.org/doc/stable", None),
+    "xarray": ("https://docs.xarray.dev/en/latest", None),
+    "pandas": ("https://pandas.pydata.org/docs/", None),
 }
